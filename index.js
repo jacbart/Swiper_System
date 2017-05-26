ebf528b9c84319cdd03638dbba433d538a6c8203
Jack Bartlett <jacbart@gmail.com>
2017-05-26 20:58:14 +0000

    Added buff png to top corner, added what the queue should look like, added customer info to start screen, and made animations synced for the start page

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 $(document).ready(function()
 {
-	var delay=1000;
+	var delay=500;
 
 	// Initial fade in of start button
-	$("#start").fadeIn("slow");
+	$("#start").show("fold");
+	setTimeout(function(){
+		$("#qpan").show("fold");
+	}, delay);
+	setTimeout(function(){
+		$("#cusinfo").show("fold");
+	}, delay+210);
+
 	$("#start").click(function()
 	{
-		$("#start").effect("fade");
+		$("#qpan").hide("blind");
+		$("#cusinfo").hide("blind");
+		$("#start").hide("blind");
 		setTimeout(function(){
-			//Fade in new buttons
-			
+			//Fade in OS selection buttons
+			$("#mac").show("blind");
+			$("#win").show("blind");
+			$("#otheros").show("blind");
 		}, delay)
 	});
 });
