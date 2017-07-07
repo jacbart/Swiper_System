d896e9d5acf39908e482cf84359206e4f09c6a8b
jacbart <jacbart@gmail.com>
2017-07-07 20:52:19 +0000

    Made the start button close during 16:30-7:59

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ $(document).ready(function()
 	// delay variable for fading in or out objects
 	var delay=600;
 
+	openClose();
+
 	// Initial fade in of start button
 	$("#start").show("fold");
 
@@ -26,14 +28,16 @@ $(document).ready(function()
 		setTimeout(function(){
 			// Fade in the Swipe page
 			$("#swipe").show("blind");
+			buffSwipe();
 		}, delay);
 	});
 
-	// Swipe Buff Onecard page
+	// Swipe Buff Onecard page - don't have card button
 	$("#identikey").click(function()
 	{
 		// Fade out swipe
 		$("#swipe").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '25%').css('width','25%');
 		setTimeout(function(){
 			// Fade in the EULA
@@ -46,6 +50,7 @@ $(document).ready(function()
 	$("#eulaNext").click(function(){
 		// Fade out EULA
 		$("#eula").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '50%').css('width','50%');
 		setTimeout(function(){
 			//Fade in OS selection buttons
@@ -61,6 +66,7 @@ $(document).ready(function()
 		$("#mac").hide("blind");
 		$("#win").hide("blind");
 		$("#otherOS").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '75%').css('width','75%');
 		setTimeout(function(){
 			// Fade in virus, email, bootfailure, and otherissue buttons
@@ -77,6 +83,7 @@ $(document).ready(function()
 		$("#mac").hide("blind");
 		$("#win").hide("blind");
 		$("#otherOS").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '75%').css('width','75%');
 		setTimeout(function(){
 			// Fade in virus, email, bootfailure, and otherissue buttons
@@ -93,6 +100,7 @@ $(document).ready(function()
 		$("#mac").hide("blind");
 		$("#win").hide("blind");
 		$("#otherOS").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '75%').css('width','75%');
 		setTimeout(function(){
 			// Fade in virus, email, bootfailure, and otherissue buttons
@@ -110,10 +118,12 @@ $(document).ready(function()
 		$("#bootfailure").hide("blind");
 		$("#email").hide("blind");
 		$("#otherIssue").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '100%').css('width','100%');
 		setTimeout(function(){
 			// Fade in fin button
-			$("#fin").show("blind")
+			$("#fin").show("blind");
+			finTimer();
 		}, delay);
 	});
 
@@ -124,10 +134,12 @@ $(document).ready(function()
 		$("#email").hide("blind");
 		$("#bootfailure").hide("blind");
 		$("#otherIssue").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '100%').css('width','100%');
 		setTimeout(function(){
 			// Fade in fin button
-			$("#fin").show("blind")
+			$("#fin").show("blind");
+			finTimer();
 		}, delay);
 	});
 
@@ -138,10 +150,12 @@ $(document).ready(function()
 		$("#bootfailure").hide("blind");
 		$("#email").hide("blind");
 		$("#otherIssue").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '100%').css('width','100%');
 		setTimeout(function(){
 			//Fade in fin button
 			$("#fin").show("blind");
+			finTimer();
 		}), delay;
 	});
 
@@ -152,10 +166,12 @@ $(document).ready(function()
 		$("#email").hide("blind");
 		$("#bootfailure").hide("blind");
 		$("#otherIssue").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '100%').css('width','100%');
 		setTimeout(function(){
 			// Fade in fin button
-			$("#fin").show("blind")
+			$("#fin").show("blind");
+			finTimer();
 		}, delay);
 	});
 
@@ -163,6 +179,7 @@ $(document).ready(function()
 	$("#fin").click(function(){
 		// Fade out fin button
 		$("#fin").hide("blind");
+		// Updating progress of theprogressbar
 		$('#theprogressbar').attr('aria-valuenow', '0%').css('width','0%');
 		setTimeout(function(){
 			// Fade in start button
@@ -178,3 +195,67 @@ $(document).ready(function()
 		}, delay);
 	});
 });
+
+function buffSwipe(){
+	var uuid = "";
+	document.onkeydown = function(event)
+	{
+		var key_code = event.keyCode;
+
+		if (95 < key_code)
+		{
+			key_code = key_code-48;
+		}	
+	
+		if (key_code == 13)
+		{
+			uuid += "-Done";
+			document.getElementById('id_out').innerHTML = uuid;
+			$("#identikey").click();
+		}
+		else
+		{
+			uuid += String.fromCharCode(key_code);
+		}
+	}
+}
+
+// If finish button is not pushed then after the set time it will auto click
+function finTimer(){
+	setTimeout(function(){
+		$("#fin").click();
+	}, 15000); // 15 second timer
+}
+
+function openClose(){
+	var minutes = new Date().getMinutes();
+	// var hours = new Date().getUTCHours() - 6;
+	var hours = new Date().getHours();
+	var day = new Date().getDay();
+
+	console.log("In Function")
+	if (day >= 1 && day <= 5)
+	{
+		if (hours >= 16 && minutes >= 30)
+		{
+			document.getElementById('start').setAttribute('disabled', 'true');
+			document.getElementById('start').innerHTML = 'Closed';
+			console.log(hours, minutes);
+		}
+		else
+		{
+			if(hours >= 8)
+			{
+				document.getElementById('start').removeAttribute('disabled');
+				document.getElementById('start').innerHTML = 'Start';
+				console.log(hours, minutes);
+			}
+		}
+	}
+	else
+	{
+		document.getElementById('start').setAttribute('disabled', 'true');
+		document.getElementById('start').innerHTML = 'Closed';
+		console.log(hours, minutes);
+	}
+}
