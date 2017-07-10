53927473532e8a009752328abe386b54c9097cb3
jacbart <jacbart@gmail.com>
2017-07-10 22:04:51 +0000

    Made selector for days of week and hours/minutes in the admin.html; updated the main.css to work with admin.hmtl

--- index.js
+++ index.js
@@ -331,3 +331,46 @@ function globalNavbar()
 	// At the beginning of the body display the navbar
 	$('body').prepend(bar);
 }
+
+// function setHours()
+// {
+// 	var now = new Date();
+// 	var weekday = new Array(7);
+// 	weekday[0] = "Sunday";
+// 	weekday[1] = "Monday";
+// 	weekday[2] = "Tuesday";
+// 	weekday[3] = "Wednesday";
+// 	weekday[4] = "Thursday";
+// 	weekday[5] = "Friday";
+// 	weekday[6] = "Saturday";
+
+
+// }
+
+function getTimes()
+{
+	var mh = document.getElementById("m-selHour").value;
+	var th = document.getElementById("t-selHour").value;
+	var wh = document.getElementById("w-selHour").value;
+	var thh = document.getElementById("th-selHour").value;
+	var fh = document.getElementById("f-selHour").value;
+	var sah = document.getElementById("sa-selHour").value;
+	var suh = document.getElementById("su-selHour").value;
+
+	var mm = document.getElementById("m-selMin").value;
+	var tm = document.getElementById("t-selMin").value;
+	var wm = document.getElementById("w-selMin").value;
+	var thm = document.getElementById("th-selMin").value;
+	var fm = document.getElementById("f-selMin").value;
+	var sam = document.getElementById("sa-selMin").value;
+	var sum = document.getElementById("su-selMin").value;
+}
+
+function setTimes()
+{
+	var t = ['mh','mm','th','tm','wh','wm','thh','thm','fh','fm','sah','sam','suh','sum'];
+	for (i = 0; i < arguments.length; i+2) {
+		document.getElementById(t[i]).value = arguments[i];
+		document.getElementById(t[i+1]).value = arguments[i+1];
+	}
+}
